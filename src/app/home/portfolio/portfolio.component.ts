3a442ca2858334a1c74d80c9f91246083c0851c0
Aidan Puricelli <aidanpuricelli@gmail.com>
2023-11-14 13:05:56 -0600

    updated

--- src/app/home/portfolio/portfolio.component.ts
+++ src/app/home/portfolio/portfolio.component.ts
@@ -11,7 +11,7 @@ export class PortfolioComponent {
     {title: 'VinylVault', role:'Creator', dates: 'Nov 2023 - Present', description: 'Developed a user-friendly vinyl collection and wishlist management application using Angular and SpringBoot.', image:'../../../assets/vinylvault.png'},
     {title: 'DivArt', role:'Creator', dates: 'Nov 2023 - Present', description: 'Website art project. All art on the website has been created using only HTML and CSS code. Inspired by Lynn Fisher\'s project, A Single Div.', image:'../../../assets/divart.png'},
     {title: 'Magic Card AI', role:'Creator', dates: 'Nov 2023 - Present', description: 'Web application created using the openAI API. After being given a prompt, generates an original Magic the Gathering card.', image:'../../../assets/magic.png'},
-    {title: 'Original Video Game', role:'Creator', dates: 'Nov 2023 - Present', description: 'Original video game created in GameMaker Studio 2. All of the art, music, and game logic has been created by me.', image:'../../../assets/videoGame.png'},
+    {title: 'Video Game', role:'Creator', dates: 'Nov 2023 - Present', description: 'Original video game created in GameMaker Studio 2. All of the art, music, and game logic has been created by me.', image:'../../../assets/videoGame.png'},
   ]
 
   scrollToElement(id: string): void {
