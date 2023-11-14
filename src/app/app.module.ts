95d79920f29ad0f0955b1dacc83733731b24a503
Aidan Puricelli <aidanpuricelli@gmail.com>
2023-11-14 12:02:10 -0600

    new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeroImgComponent } from './home/hero-img/hero-img.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './home/portfolio/portfolio.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeroImgComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
